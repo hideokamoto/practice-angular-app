962c1eadea8cf91aba9f481a8efedf5e89e62573
Hidetaka Okamoto <kokkoku214@gmail.com>
2022-07-20 19:32:40 +0900

    remove module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from '../in-memory-data.service';
+import { HeroService } from '../shares/hero/hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroService } from '../shares/hero/hero.service';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
